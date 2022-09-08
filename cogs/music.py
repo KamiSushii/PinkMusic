210f0cf89787bbd9a2f23c1e56861c269617b286
Sushi <perdana.0228@gmail.com>
2022-09-08 13:11:49 +0700

    Minimum functional music bot

--- cogs/music.py
+++ cogs/music.py
@@ -1,12 +1,32 @@
+import wavelink
 from discord.ext import commands
 
 class Music(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        bot.loop.create_task(self.start_nodes())
+    
+    async def start_nodes(self):
+        await self.bot.wait_until_ready()
+        await wavelink.NodePool.create_node(bot=self.bot,
+                                            host='lava.link',
+                                            port=80,
+                                            password='youshallnotpass',
+                                            identifier='pinkMusic',
+                                            region='asia',)
 
-    @commands.command(name="test")
-    async def test(self, ctx):
-        await ctx.send("tested")
+    @commands.Cog.listener()
+    async def on_wavelink_node_ready(self, node: wavelink.Node):
+        print(f'Node: <{node.identifier}> is ready!')
+
+    @commands.command()
+    async def play(self, ctx: commands.Context, *, search: wavelink.YouTubeTrack):
+        if not ctx.voice_client:
+            vc: wavelink.Player = await ctx.author.voice.channel.connect(cls=wavelink.Player)
+        else:
+            vc: wavelink.Player = ctx.voice_client
+
+        await vc.play(search)
 
 def setup(bot):
     bot.add_cog(Music(bot))
